09607ba2356f2c99350cbb93a0ca19fa21ed15c3
aivxx <90653195+aivxx@users.noreply.github.com>
2021-11-24 15:08:50 -0500

    create raycast

--- Assets/Scripts/PlayerToMoon.cs
+++ Assets/Scripts/PlayerToMoon.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerToMoon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    // object we're placing on the surface
+    public Transform objectToPlace;
+
+
+
+    // This function is called every fixed framerate frame, if the MonoBehaviour is enabled
+    private void FixedUpdate()
     {
-        
+        // hit variable to store our results in
+        RaycastHit hit;
+
+        // ray from the camera to world
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Conduct raycast
+        if (Physics.Raycast(ray, out hit))
+        {
+            // manipulate the object to place
+            objectToPlace.position = hit.point;
+            // rotate the object to be right side up
+            objectToPlace.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
